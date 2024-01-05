a16856b1ac8e019568b922e7f7c114a227ce7055
vmanh131 <macemi147@gmail.com>
2024-01-05 20:14:43 +0700

    change

--- main.cc
+++ main.cc
@@ -19,7 +19,7 @@ int main()
      // 2. Host - CPU Network
     std::cout << "Test: Host - CPU Network" << std::endl;
     Network dnn1 = createNetwork_CPU();
-    dnn1.load_parameters("./model/weights-cpu-trained.bin");
+    dnn1.load_parameters("./model/weights_cpu.bin");
     dnn1.forward(dataset.test_data);
     accuracy = compute_accuracy(dnn1.output(), dataset.test_labels);
     std::cout << "test accuracy: " << accuracy << std::endl;
@@ -27,7 +27,7 @@ int main()
 
     // 3. Device - GPU Network
     Network dnn2 = createNetwork_GPU();
-    dnn2.load_parameters("./model/weights-cpu-trained.bin");
+    dnn2.load_parameters("./model/weights_cpu.bin");
     dnn2.forward(dataset.test_data);
     accuracy = compute_accuracy(dnn2.output(), dataset.test_labels);
     std::cout << "test accuracy: " << accuracy << std::endl;
